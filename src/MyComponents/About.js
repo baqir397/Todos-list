1c06efc2802e74c5c30b56e66a96c7a81b0ae091
Baqir Alvi <baqiralvi397@github.com>
2021-06-26 15:05:17 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h2 className="my-3">About Todo List</h2>
+            <p>It's a list of tasks you need to complete, or things that you want to do. Most typically, they're organised in order of priority. 
+                Traditionally, they're written on a piece of paper or post it notes and acts as a memory aid.</p>
+            <p> A close second is that is this: a to-do list helps prevent your most urgent things from slipping through the cracks—it prevents 
+                you from dropping any major balls</p>
+            <p>Have more than one list. If you are the kind of person that cannot remember things if they are not written down, then it is worth 
+                having two (or more) lists: ...
+                Keep your daily or weekly lists manageable. ...
+                Keep your list up to date.</p>    
+        </div>
+    )
+}
